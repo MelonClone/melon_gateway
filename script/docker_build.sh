ca793a938f2c02101913b77bd1850fbcafe36cae
gdmail91 <gdmail91@gmail.com>
2020-07-15 18:01:17 +0900

    Fix repo name

--- script/docker_build.sh
+++ script/docker_build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 ./gradlew clean build
-docker build -t melon-gateway-server:latest .
+docker build -t melon-gateway:latest .
 docker container stop melon-gateway
 docker container rm melon-gateway
-docker run --ip 172.19.0.111 -p 8765:8765 --name melon-gateway melon-gateway-server:latest
+docker run -d --net mynet --ip 172.19.0.111 -p 8765:8765 --name melon-gateway melon-gateway:latest
